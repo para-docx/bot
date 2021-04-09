c87414eaa03a4e27b7c17290023adf07f23737a6
para-docx <72385299+para-docx@users.noreply.github.com>
2021-04-09 15:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
 import discord
-import os
 import requests 
 import json
+from discord.ext import commands, tasks
+from random import choice
+import aiohttp
 
-client = discord.Client()
 
-keyword = ["sus"]
+client = commands.Bot(command_prefix="#")
 
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+status = ["sus","hey hey","your so sussy", "19$ fortnite card"]
 
 
 @client.event
 async def on_ready():
-  print ('we have logged in as {0.user}'.format(client) )
+  change_status.start()
+  print ('we have logged in as {0.user}'.format(client))
+
+
+@client.command(name= 'inspire', help='you need it')
+async def inspire(ctx):
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    await ctx.send(quote)
+
+@client.command(name='tiger') 
+async def tiger(ctx):
+    await ctx.send('The Mama Bean')    
+
+@client.command(name='skugg') 
+async def skugg(ctx):
+    await ctx.send('Aeyo Sexy Man')
+
+@client.command(name='jedi') 
+async def jedi(ctx):
+    await ctx.send('Dripped out in swag, flooded in sadness') 
+
+@client.command(name='crimsonwolf') 
+async def crimsonwolf(ctx):
+    await ctx.send('Aeyo its the wolf :wolfpoggers:go check him out at twitch.tv/twitch_chase05')  
+                   
+
+@client.command(name= 'dogpics', help='you need dog pics just use it')
+async def dogpics(ctx):
+    async with aiohttp.ClientSession() as session:
+        requests = await session.get('https://some-random-api.ml/img/dog') #requests
+        dogjson = await requests.json() #json file comversion
+    embed = discord.Embed(title = "heres a cute dog pic for you!!!", color=discord.Colour.purple())    
+    embed.set_image(url=dogjson['link'])
+    await ctx.send(embed=embed) #send message
 
 
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
-  if message.content.startswith('$springtrap'):
-    await message.channel.send('yo that furry who killed 5 children at a pizzeria')
-  
-  if message.content.startswith('$motivate'):
-    quote = get_quote()
-    await message.channel.send(quote)
+@tasks.loop(seconds=20)
+async def change_status():
+	await client.change_presence(activity=discord.Game(choice(status)))
 
-  if message.content.startswith('$jedi'):
-    await message.channel.send(' Dripped out in swag, flooded in sadness')  
-
-  if message.content.startswith('$exokn'):
-   await message.channel.send('shut up jedi-prime was way better')
-  
-  if message.content.startswith('$tiger'):
-   await message.channel.send('the mama bean')
-
-  if message.content.startswith('$Bam'):
-   await message.channel.send('Vroom is best') 
-
-  if message.content.startswith('$skugg'):
-   await message.channel.send('Yo SEXY man')  
-  
-  
-@client.event
-async def my_message(message):
-  for i in range(len(keyword)):
-    if keyword in message.content:
-      for j in range(10):
-        await message.channel.sent('STOP POSTING ABOUT AMONG US,IM TIRED OF SEEING THIS')
   
 
 client.run(os.getenv('token'))
